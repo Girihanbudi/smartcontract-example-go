397bf04aed790a2c2ce17dc431f927f2847d09b0
girihanbudi <Ghanbudi@gmail.com>
2023-06-04 21:35:29 +0700

    splitted function for more flexible use

--- internal/pkg/ethclient/trxopt.go
+++ internal/pkg/ethclient/trxopt.go
@@ -7,29 +7,63 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-func (c *Client) GetTrxOpts(ctx context.Context, address string) (trxOpts *bind.TransactOpts, nonce uint64, err error) {
-	privateKey, err := crypto.HexToECDSA(address)
+func (c *Client) GenerateKey(privateKeyAddress string) (addr common.Address, privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey, err error) {
+	privateKey, err = crypto.HexToECDSA(privateKeyAddress)
 	if err != nil {
 		err = fmt.Errorf("failed to parse address as private key: %s", err.Error())
 		return
 	}
 
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	cryptoPublicKey := privateKey.Public()
+	publicKey, ok := cryptoPublicKey.(*ecdsa.PublicKey)
 	if !ok {
 		err = fmt.Errorf("invalid provided key")
 		return
 	}
 
-	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	addr = crypto.PubkeyToAddress(*publicKey)
+	return
+}
 
+func (c *Client) GetNonce(ctx context.Context, address common.Address) (nonce uint64, err error) {
 	//fetch the last use nonce of account
-	nonce, err = c.Client.PendingNonceAt(ctx, fromAddress)
+	nonce, err = c.Client.PendingNonceAt(ctx, address)
 	if err != nil {
 		err = fmt.Errorf("failed to get pending nonce, %s", err.Error())
+		return
+	}
+
+	return
+}
+
+func (c *Client) GetNonceFromPrivateKey(ctx context.Context, privateKeyAddress string) (nonce uint64, err error) {
+	addr, _, _, err := c.GenerateKey(privateKeyAddress)
+	if err != nil {
+		return
+	}
+
+	//fetch the last use nonce of account
+	nonce, err = c.Client.PendingNonceAt(ctx, addr)
+	if err != nil {
+		err = fmt.Errorf("failed to get pending nonce, %s", err.Error())
+		return
+	}
+
+	return
+}
+
+func (c *Client) GetTrxOpts(ctx context.Context, privateKeyAddress string) (trxOpts *bind.TransactOpts, err error) {
+	addr, privateKey, _, err := c.GenerateKey(privateKeyAddress)
+	if err != nil {
+		return
+	}
+
+	nonce, err := c.GetNonce(ctx, addr)
+	if err != nil {
 		return
 	}
 
@@ -45,20 +79,19 @@ func (c *Client) GetTrxOpts(ctx context.Context, address string) (trxOpts *bind.
 		return
 	}
 
-	return trxOpts, nonce, nil
+	trxOpts.Nonce = big.NewInt(int64(nonce))
+
+	return trxOpts, nil
 }
 
-func (c *Client) SetTrxOptsVal(trxOpts *bind.TransactOpts, nonce uint64, fund int64, gasLimit uint64, gasPrice int64) *bind.TransactOpts {
+func (c *Client) SetTrxOptsVal(trxOpts *bind.TransactOpts, fund int64, gasLimit uint64, gasPrice int64) {
 	if trxOpts != nil {
-		trxOpts.Nonce = big.NewInt(int64(nonce))
 		trxOpts.Value = big.NewInt(c.DefaultFund) // in wei
 		trxOpts.GasLimit = c.DefaultGasLimit      // in units
 		trxOpts.GasPrice = big.NewInt(c.DefaultGasPrice)
 	}
-
-	return trxOpts
 }
 
-func (c *Client) SetWithDefaultTrxOptsVal(trxOpts *bind.TransactOpts, nonce uint64) *bind.TransactOpts {
-	return c.SetTrxOptsVal(trxOpts, nonce, c.Options.DefaultFund, c.Options.DefaultGasLimit, c.Options.DefaultGasPrice)
+func (c *Client) SetWithDefaultTrxOptsVal(trxOpts *bind.TransactOpts) {
+	c.SetTrxOptsVal(trxOpts, c.Options.DefaultFund, c.Options.DefaultGasLimit, c.Options.DefaultGasPrice)
 }

dce6f8b3ebdcf2e0ac686150dd0902d6e947656b
girihanbudi <Ghanbudi@gmail.com>
2023-06-04 21:34:50 +0700

    updated eth client config

--- internal/pkg/ethclient/config/config.go
+++ internal/pkg/ethclient/config/config.go
@@ -2,6 +2,8 @@ package config
 
 type Config struct {
 	Host            string `mapstructure:"host"`
+	SmHolder        string `mapstructure:"smartcontractholder"`
+	SmAddr          string `mapstructure:"smartcontractaddress"`
 	DefaultFund     int64  `mapstructure:"defaultfund"`
 	DefaultGasLimit uint64 `mapstructure:"defaultgaslimit"`
 	DefaultGasPrice int64  `mapstructure:"defaultgasprice"`
